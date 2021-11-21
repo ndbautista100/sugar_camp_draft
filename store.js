e09b95402f127644141331f037076c1fe36842b8
ndbautista100 <47700739+ndbautista100@users.noreply.github.com>
2021-11-21 11:24:25 -0800

    remove function

--- store.js
+++ store.js
@@ -1,21 +1,25 @@
 let carts = document.querySelectorAll('.add-cart');
 
 
+
 let products = [
     {
         name: 'Betsy Vintage Tee',
         price: 15.99,
-        inCart: 0
+        inCart: 0,
+        stock: 1
     },
     {
         name: 'Temptations Cafe Tee',
         price: 15.99,
-        inCart: 0
+        inCart: 0,
+        stock: 1
     },
     {
         name: 'SugarCamp Stickers',
         price: 2.99,
-        inCart: 0
+        inCart: 0,
+        stock: 5
     }
 ];
 
@@ -54,8 +58,9 @@ function cartNumbers(product){
 function setItems(product){
     console.log("My product is", product);
     let cartItems = localStorage.getItem('productsInCart');
-
+    
     cartItems = JSON.parse(cartItems);
+    
 
     if(cartItems != null){
 
@@ -87,6 +92,35 @@ function totalCost(product){
     }
 }
 
+function remove(product){
+    const EL_button = event.currentTarget;
+    //const id = this.closest("span"); // i.e: "245"
+    var span_Text = document.getElementById("span_id").innerText;
+    var span_Price = document.getElementById("span_price").innerText;
+    console.log(span_Price);
+    console.log("before : ", window.localStorage.productsInCart);  //display it
+    var favs = JSON.parse(window.localStorage.productsInCart || {});  //read and convert to object
+    var cartNum = JSON.parse(window.localStorage.cartNumbers || {});
+    var cost = parseInt(JSON.parse(window.localStorage.totalCost || {}));
+    if (favs[span_Text]) {  //check if key exists
+        delete favs[span_Text];  //remove the key from object
+        cartNum = cartNum - 1;
+        cost = Math.round(cost - span_Price);
+    }
+    window.localStorage.productsInCart = JSON.stringify(favs);  //save it back
+    window.localStorage.cartNumbers = JSON.stringify(cartNum);
+    window.localStorage.totalCost = JSON.stringify(cost);
+    console.log("after : ", window.localStorage.productsInCart);
+    console.log("after : ", window.localStorage.cartNumbers);
+    console.log("after : ", window.localStorage.totalCost);
+    window.location.reload();
+    
+
+    
+    
+    
+}
+
 function displayCart(){
     let cartItems = localStorage.getItem("productsInCart");
     cartItems = JSON.parse(cartItems);
@@ -97,11 +131,16 @@ function displayCart(){
     if( cartItems && productContainer ){
         productContainer.innerHTML = '';
         Object.values(cartItems).map(item =>{
-            productContainer.innerHTML += `
+            if(item.stock > 1){
+                productContainer.innerHTML += `
             <div class="product">
-                <ion-icon name="close-circle-outline"></ion-icon>
                 <img src="./images/${item.name}.jpg">
                 <span>${item.name}</span>
+                    <ion-icon name="close-circle-outline" onClick="remove(this)">
+                    <span id="span_id" style="display:none">${item.name}</span>
+                    <span id="span_price" style="display:none">${item.price}</span>
+                    </ion-icon>
+                
             </div>
             <div class="price">$${item.price}</div>
             <div class="quantity">
@@ -113,6 +152,25 @@ function displayCart(){
             <div class="total">
                 $${item.inCart * item.price} 
             </div>`
+            }else{
+            productContainer.innerHTML += `
+            <div class="product">
+                <img src="./images/${item.name}.jpg">
+                <span>${item.name}</span>
+                    <ion-icon name="close-circle-outline" onClick="remove(this)">
+                    <span id="span_id" style="display:none">${item.name}</span>
+                    <span id="span_price" style="display:none">${item.price}</span>
+                    </ion-icon>
+                
+            </div>
+            <div class="price">$${item.price}</div>
+            <div class="quantity">
+                <span>${item.inCart}</span>
+                
+            </div>
+            <div class="total">
+                $${item.inCart * item.price} 
+            </div>`}
         });
         productContainer.innerHTML += `
         <div class="basketTotalContainer">
